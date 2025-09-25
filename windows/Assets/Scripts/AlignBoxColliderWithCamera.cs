5ce0698216b1f888ae213cae0740c29999191bb0
Mark Colley <39366189+M-Colley@users.noreply.github.com>
2025-09-25 16:14:48 +0200

    Update AlignBoxColliderWithCamera.cs

--- windows/Assets/Scripts/AlignBoxColliderWithCamera.cs
+++ windows/Assets/Scripts/AlignBoxColliderWithCamera.cs
@@ -57,12 +57,6 @@ public class AlignBoxColliderWithCamera : MonoBehaviour
 
     private void MatchPlaneToScreenSize()
     {
-        /*
-        float planeToCameradistance = Vector3.Distance(boxCollider.transform.position, camera.transform.position);
-        float planeHeightScale = (2.0f * Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad) * planeToCameradistance) / 2;
-        camera.orthographicSize = planeHeightScale;
-        */
-        // Ensure the camera is orthographic
         // Ensure the camera is orthographic
         camera.orthographic = true;
 
@@ -73,7 +67,10 @@ public class AlignBoxColliderWithCamera : MonoBehaviour
         float boxHeight = bounds.size.y;
 
         // Determine the taskbar height in pixels (you can set this value based on the screen resolution and taskbar size)
-        int taskbarHeightPixels = 48; // Default medium size for a full HD screen
+        // Get the display information for the main display.
+        DisplayInfo mainDisplay = Screen.mainWindowDisplayInfo;
+        // Calculate the taskbar height.
+        int taskbarHeightPixels = mainDisplay.height - mainDisplay.workArea.height;
 
         // Get the screen height in pixels
         int screenHeightPixels = Screen.height;
